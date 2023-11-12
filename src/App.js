1780cdfafbf9327fa8dfe328ca7f4c093718c295
Shalini  V <105047114+shaal123@users.noreply.github.com>
2023-11-13 03:04:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import logo from "./logo.png";
+export default function App() {
+  return (
+    <div className="main">
+      <div className="header-con">
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  );
+}
